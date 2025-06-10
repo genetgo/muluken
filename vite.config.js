1061442fedc65d5c398249030685df956e0df60e
genetgo <enatemaryam796@gmail.com>
2025-06-10 22:06:30 +0300

    Fix Netlify routing with _redirects and base path

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/muluken/',  // 👈 Set to your GitHub repo name with slashes
+  base: '/', 
 })
